cfdea47b1c2fcb5b155e2af580aa36a7b8db3e35
yustas147 <yustas@simbioz.com.ua>
2016-12-27 18:15:48 +0200

    start

---
+++ __openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hr_skills_extended",
+
+    'summary': """
+        Adds 2 fields to hr_skills.hr_skill model
+"""
+
+    'description': """
+        Adds 2 fields to hr_skills.hr_skill model: Percentage and Description
+    """,
+
+    'author': "Simbioz",
+    'website': "http://simbioz.odooua.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'hr',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr_skills'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+     #   'templates.xml',
+        'views/inherited_hr_skill_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+     #   'demo.xml',
+    ],
+}
---
+++ models/hr_skills_inherited.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class hr_skills_extended(models.Model):
+#     _name = 'hr_skills_extended.hr_skills_extended'
+
+#     name = fields.Char()
