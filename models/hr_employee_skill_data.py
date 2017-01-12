640e9a12cc4e3ff1a5b9cbcec96aac5f8b50c3a8
yustas147 <yustas@simbioz.com.ua>
2017-01-12 15:53:43 +0200

    bar worked

--- models/hr_employee_skill_data.py
+++ models/hr_employee_skill_data.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+import logging
+
+_logger = logging.getLogger(name='YUSTAS')
 
 class hr_employee_skill_data(models.Model):
     _name = 'hr.employee.skill.data'    
     
-    name = fields.Char('Name')
+    #name = fields.Char('Name')
     percentage = fields.Integer(string='Grade of owning')
     description = fields.Text(string='Full skill owning employee description')    
     employee_id = fields.Many2one(comodel_name='hr.employee', string=None)
     skill_id = fields.Many2one(comodel_name='hr.skill', string=None)
     
     @api.model
-    @api.multi
-    def sync_skills(self):
-        emp = self.employee_id
-        emp.update({"skill_ids":[(4,self.skill_id.id)]})
+    def sync_skills(self,obj):
+        _logger.info('self is %s' % (unicode(self)))
+        emp = obj.employee_id
+        emp.update({"skill_ids":[(4,obj.skill_id.id)]})
         
 class hr_employee(models.Model):
     _name = 'hr.employee'    
