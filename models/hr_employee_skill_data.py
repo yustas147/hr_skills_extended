362491aaaf89c615c59c48112823977699820eb1
yustas147 <yustas@simbioz.com.ua>
2017-01-13 11:52:16 +0200

    1a

--- models/hr_employee_skill_data.py
+++ models/hr_employee_skill_data.py
@@ -18,8 +18,21 @@ class hr_employee_skill_data(models.Model):
     def sync_skills(self,obj):
         _logger.info('self is %s' % (unicode(self)))
         emp = obj.employee_id
-        emp.update({"skill_ids":[(4,obj.skill_id.id)]})
+#        emp.update({"skill_ids":[(4,obj.skill_id.id)]})
+        sk_ids = emp.hr_employee_skill_data_ids.mapped('skill_id').mapped('id')
+        _logger.info('sk_ids is %s' % (sk_ids)) 
+        emp.update({"skill_ids":[(6,0,sk_ids)]})
         
+        
+class hr_skill(models.Model):
+    _name = 'hr.skill'    
+    _inherit = 'hr.skill'    
+    
+    hr_employee_skill_data_ids = fields.One2many(comodel_name='hr.employee.skill.data', 
+                                                inverse_name='skill_id', 
+                                                string='Employee data')                                                
+    
+    
 class hr_employee(models.Model):
     _name = 'hr.employee'    
     _inherit = 'hr.employee'    
@@ -27,3 +40,13 @@ class hr_employee(models.Model):
     hr_employee_skill_data_ids = fields.One2many(comodel_name='hr.employee.skill.data', 
                                                 inverse_name='employee_id', 
                                                 string='Skills')                                                
+                                                
+    @api.model
+    def sync_skills_2(self,obj):
+        _logger.info('self is %s' % (unicode(self)))
+        emp = obj
+#        emp.update({"skill_ids":[(4,obj.skill_id.id)]})
+        sk_ids = emp.hr_employee_skill_data_ids.mapped('skill_id').mapped('id')
+        _logger.info('sk_ids is %s' % (sk_ids)) 
+        emp.update({"skill_ids":[(6,0,sk_ids)]})
+    
