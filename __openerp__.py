16ce7347535ae61659d8413b13ceb85fcfdc79ae
yustas147 <yustas@simbioz.com.ua>
2017-03-29 16:32:22 +0300

    rules

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr_skill','hr_ext_qa'],
+    'depends': ['base','hr_skill'],
 
     # always loaded
     'data': [
